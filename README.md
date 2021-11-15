f53a6fa62ee9cb5610cea1ad15e49eb4813167ec
Zdravko <zdravkokirilov@yahoo.com>
2021-11-15 15:16:16 +0200

    add searching

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ and then
 npm start
 ```
 
-then open localhost:8000 and "Hello world" should be returned
+then open localhost:8080 and "Hello world" should be returned
 
 ## Endpoints
 
@@ -23,16 +23,23 @@ then open localhost:8000 and "Hello world" should be returned
 
 2. /bears/1 - will return the first bear from the list, there are 4 bears in total. Also randomly fails 10% of the time. Trying to search for a non-existing bear, e.g. - /bears/7 always returns an error
 
-## Accessing from front end apps
+3. /bears/search?name=some_search_string - returns a list of matched bears ( by their name)
 
+## Accessing from front end apps
 
 1. Returns all bears:
 
-    ```js
-    fetch('http://localhost:8000/bears')
-    ```
+   ```js
+   fetch("http://localhost:8080/bears");
+   ```
 
 2. Returns a single bear
-    ```js
-    fetch('http://localhost:8000/bears/2');
-    ```
+
+   ```js
+   fetch("http://localhost:8080/bears/2");
+   ```
+
+3. Searches for a bear by name
+   ```js
+   fetch("http://localhost:8080/bears/search?name=polar}");
+   ```
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
-const lodash = require('lodash');
+const lodash = require("lodash");
 
 const app = express();
 
@@ -8,42 +8,37 @@ app.use(cors());
 
 const bears = [
   {
-    "id": 1,
-    "name": "Brown bear",
-    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/2010-kodiak-bear-1.jpg/330px-2010-kodiak-bear-1.jpg",
-    "latinName": "Ursus arctos",
-    "habitat": [
-      "Eurasia",
-      "North America"
-    ]
+    id: 1,
+    name: "Brown bear",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/2010-kodiak-bear-1.jpg/330px-2010-kodiak-bear-1.jpg",
+    latinName: "Ursus arctos",
+    habitat: ["Eurasia", "North America"],
   },
   {
-    "id": 2,
-    "name": "Polar bear",
-    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Polar_Bear_-_Alaska_%28cropped%29.jpg/330px-Polar_Bear_-_Alaska_%28cropped%29.jpg",
-    "latinName": "Ursus maritimus",
-    "habitat": [
-      "Arctic circle"
-    ]
+    id: 2,
+    name: "Polar bear",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Polar_Bear_-_Alaska_%28cropped%29.jpg/330px-Polar_Bear_-_Alaska_%28cropped%29.jpg",
+    latinName: "Ursus maritimus",
+    habitat: ["Arctic circle"],
   },
   {
-    "id": 3,
-    "name": "American black bear",
-    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/01_Schwarzb%C3%A4r.jpg/330px-01_Schwarzb%C3%A4r.jpg",
-    "latinName": "Ursus americanus",
-    "habitat": [
-      "North America"
-    ]
+    id: 3,
+    name: "American black bear",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/01_Schwarzb%C3%A4r.jpg/330px-01_Schwarzb%C3%A4r.jpg",
+    latinName: "Ursus americanus",
+    habitat: ["North America"],
   },
   {
-    "id": 4,
-    "name": "Giant panda",
-    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Grosser_Panda.JPG/330px-Grosser_Panda.JPG",
-    "latinName": "Ailuropoda melanoleuca",
-    "habitat": [
-      "China"
-    ]
-  }
+    id: 4,
+    name: "Giant panda",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Grosser_Panda.JPG/330px-Grosser_Panda.JPG",
+    latinName: "Ailuropoda melanoleuca",
+    habitat: ["China"],
+  },
 ];
 
 const randomFailure = () => {
@@ -56,35 +51,56 @@ const generateRandomDelay = () => {
   return [100, 300, 600, 1100, 2000][random];
 };
 
-app.get('/', (req, res) => {
-  return res.send('Hello world');
+app.get("/", (req, res) => {
+  return res.send("Hello world");
 });
 
-app.get('/bears', (req, res) => {
-  const leanBears = bears.map(bear => lodash.omit(bear, ['latinName', 'habitat']));
+app.get("/bears", (req, res) => {
+  const leanBears = bears.map((bear) =>
+    lodash.omit(bear, ["latinName", "habitat"])
+  );
   const randomDelay = generateRandomDelay();
 
   setTimeout(() => {
-    return randomFailure() ? res.status(500).send('Unexpected error') : res.json(leanBears);
+    return randomFailure()
+      ? res.status(500).send("Unexpected error")
+      : res.json(leanBears);
   }, randomDelay);
-
 });
 
-app.get('/bears/:bearId', (req, res) => {
+app.get("/bears/search", (req, res) => {
+  const query = req.query.name || "";
+  const lowerCaseQuery = query.toLowerCase();
+
+  const leanBears = bears
+    .filter(
+      (bear) =>
+        lowerCaseQuery && bear.name.toLowerCase().includes(lowerCaseQuery)
+    )
+    .map((bear) => lodash.omit(bear, ["latinName", "habitat"]));
+  const randomDelay = generateRandomDelay();
+
+  setTimeout(() => {
+    return randomFailure()
+      ? res.status(500).send("Unexpected error")
+      : res.json(leanBears);
+  }, randomDelay);
+});
+
+app.get("/bears/:bearId", (req, res) => {
   const bearId = Number(req.params.bearId);
   const randomDelay = generateRandomDelay();
-  const bear = bears.find(bear => bear.id === bearId);
+  const bear = bears.find((bear) => bear.id === bearId);
 
   setTimeout(() => {
     if (bear) {
-      return randomFailure() ? res.status(500).send('Unexpected error') : res.json(bear);
+      return randomFailure()
+        ? res.status(500).send("Unexpected error")
+        : res.json(bear);
     }
 
-    return res.status(404).send('Bear not found');
+    return res.status(404).send("Bear not found");
   }, randomDelay);
-  
 });
 
-app.listen(8080, () =>
-  console.log(`Bear API listening on port 8080!`),
-);
+app.listen(8080, () => console.log(`Bear API listening on port 8080!`));
